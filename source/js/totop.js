288465c68cab54f15073208c8ca062a83d2d65e0
xiaoyan-sun <sunmike118@gmail.com>
2016-08-22 21:57:12 +0800

    增加前端搜索，后续有待优化

--- source/js/totop.js
+++ source/js/totop.js
@@ -1,4 +1,26 @@
-(function($) { 
+// 增加一部分用于固定右半部分  aside div
+
+window.onscroll = function(){
+	var scrollTopVal = document.body.scrollTop!=0? document.body.scrollTop : document.documentElement.scrollTop;
+	var asidebar = document.getElementById("sidebar");
+	//alert(scrollTopVal)
+	if (scrollTopVal >= 200){
+		asidebar.style.position = "fixed";
+		//asidebar.style.top = "100px";
+		asidebar.style.left = "76.5%";
+		asidebar.style.width = "20%";
+		//asidebar.style.right = 3%;
+	}
+	else{
+		asidebar.style.position = "relative";
+		asidebar.style.top=0;
+		asidebar.style.left = 0;
+		asidebar.style.width = "22%"
+	}
+		
+};
+
+/*(function($) { 
 	// When to show the scroll link
 	// higher number = scroll link appears further down the page：显示返回顶部的位置
 	var upperLimit = 1000;
@@ -25,3 +47,5 @@
 		$('html, body').animate({scrollTop:0}, scrollSpeed); return false;
 	});
 })(jQuery);
+
+*/
